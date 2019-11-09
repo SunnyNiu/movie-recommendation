32a8b15e51eb92702304229d7ee6e0c7a8706bf4
Sunny Niu <sunnyyingli@gmail.com>
2019-11-10 12:29:22 +1300

    fix proxy setting and remove bundle.js.map

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
     contentBase: './server/public',
     proxy: {
       '/movie/*': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:3000',
         secure: false
       }
     }
