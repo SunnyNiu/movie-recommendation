f01c6653d0d18872059edfd13ab0103bc5c58bb7
Sunny Niu <sunnyyingli@gmail.com>
2019-11-17 13:06:31 +1300

    tried

--- redux/movie.actions.js
+++ redux/movie.actions.js
@@ -8,24 +8,10 @@ function updateMovieId (movie) {
   }
 }
 
-// export function fetchMovie () {
-//   return function (dispatch) {
-//     return request.get('http://localhost:3000/movie')
-//       .then(resp => resp.json())
-//       .then((body) => { console.log('body', body); dispatch(updateMovieId(body)) })
-//   }
-// }
-
 export function fetchMovie () {
-  return (dispatch) => {
-    request
-      .get(`/movie`)
-      .end((err, res) => {
-        console.log('action', res.json())
-        if (err) {
-          console.log(err)
-        }
-        dispatch(updateMovieId(res.json()))
-      })
+  return function (dispatch) {
+    return fetch('http://localhost:3000/movie')
+      .then(resp => resp.json())
+      .then((body) => { console.log('body', body); dispatch(updateMovieId(body)) })
   }
 }
