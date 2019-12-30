5e3da8ecb69bf7b525940416bc811f7c6fc36234
Sunny Niu <sunnyyingli@gmail.com>
2019-12-31 11:01:55 +1300

    fix routes tests

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,41 +2,19 @@ const request = require('supertest')
 
 const server = require('../server/server')
 
-const mockGetGenresIdByGenre = [{
-  id: 1,
-  type: 'Action'
-}, {
-  id: 2,
-  type: 'Adventure'
-}]
-
 const mockGetMovieById = { id: 2, name: 'Test2', image: './image1.png' }
 
-const mockGetMovieIdByGenreId = [
-  { id: 4, movie_id: 3, genre_id: 2 }
-]
+const mockGetMoviesByChosedTypes = { id: 2, name: 'Test2', image: './image1.png' }
 
-const mockGetMovieIdByGenreIdNotIncludedBefore = [
-  { id: 4, movie_id: 3, genre_id: 2 }
-]
-
-const mockGetGenresId = [
-  { id: 4, movie_id: 3, genre_id: 2 },
-  { id: 6, movie_id: 3, genre_id: 6 }
-]
-
-const mockGetGenresByIds = [
+const mockGetGenresByMovieId = [
   { id: 4, type: 'Biography' },
   { id: 6, type: 'SciFi' }
 ]
 
 jest.mock('../db/db', () => ({
-  getGenresIdByGenre: (genre) => Promise.resolve(mockGetGenresIdByGenre),
   getMovieById: (movieId) => Promise.resolve(mockGetMovieById),
-  getMovieIdByGenreId: (genreId) => Promise.resolve(mockGetMovieIdByGenreId),
-  getMovieIdByGenreIdNotIncludedBefore: (genreId, movieIds) => Promise.resolve(mockGetMovieIdByGenreIdNotIncludedBefore),
-  getGenresId: (movieId) => Promise.resolve(mockGetGenresId),
-  getGenresByIds: (genreIds) => Promise.resolve(mockGetGenresByIds)
+  getMoviesByChosedTypes: (genre, moviesId) => Promise.resolve(mockGetMoviesByChosedTypes),
+  getGenresByMovieId: (genreIds) => Promise.resolve(mockGetGenresByMovieId)
 }))
 
 describe('get random movie', () => {
@@ -54,7 +32,7 @@ describe('get movie genres by movieId', () => {
     return request(server)
       .get('/movieGenres/3')
       .then((res) => {
-        expect(res.body).toEqual(mockGetGenresByIds)
+        expect(res.body).toEqual(mockGetGenresByMovieId)
       })
   })
 })
@@ -64,7 +42,7 @@ describe('get recommended movie base on choosed movies', () => {
     return request(server)
       .get('/recommendation/4/2')
       .then((res) => {
-        expect(res.body).toEqual(mockGetMovieById)
+        expect(res.body).toEqual(mockGetMoviesByChosedTypes)
       })
   })
 })
