2b8336af8b8d762e34145af503f57eb13e3b1e0a
Sunny Niu <sunnyyingli@gmail.com>
2020-01-03 11:45:20 +1300

    updated db tests due to changes of db functions

--- tests/db/db.test.js
+++ tests/db/db.test.js
@@ -11,23 +11,13 @@ beforeEach(() => {
 afterEach(() => env.cleanup(testDb))
 
 describe('movies database tests', () => {
-  it('getGenresIdByGenre returns genre by genre', () => {
-    expect.assertions(1)
-    const expected = [{ id: 1, type: 'Action' }]
-
-    return db.getGenresIdByGenre('Action', testDb)
-      .then(actual =>
-        expect(actual).toEqual(expected)
-      )
-  })
-
   it('getMovieById returns the correct movie', () => {
     expect.assertions(1)
 
     const movieId = '2'
     const expected = {
       id: 2,
-      name: 'The Incredible Hulk (2008)',
+      name: 'The Incredible Hulk',
       image: '/images/2.png'
     }
 
@@ -37,83 +27,19 @@ describe('movies database tests', () => {
       )
   })
 
-  it('getMovieIdByGenreId returns the correct movieId', () => {
+  it('getMoviesByIds returns the correct movies', () => {
     expect.assertions(1)
 
-    const genreId = '6'
-    const expected = [{ id: 3, movie_id: 1, genre_id: 6 }, { id: 6, movie_id: 2, genre_id: 6 }]
-
-    return db.getMovieIdByGenreId(genreId, testDb)
-      .then(actual =>
-        expect(actual).toEqual(expected)
-      )
-  })
-
-  it('getMovieIdByGenreIdNotIncludedBefore returns the correct movieId', () => {
-    expect.assertions(1)
-
-    const genreId = '6'
-    const movieIds = [1, 4]
-    const expected = [{ id: 6, movie_id: 2, genre_id: 6 }]
-
-    return db.getMovieIdByGenreIdNotIncludedBefore(genreId, movieIds, testDb)
-      .then(actual =>
-        expect(actual).toEqual(expected)
-      )
-  })
-
-  it('getGenresId returns the correct genresId by movieId', () => {
-    expect.assertions(1)
-
-    const movieId = 1
-    const expected = [{ id: 1, movie_id: 1, genre_id: 1 },
-      { id: 2, movie_id: 1, genre_id: 2 },
-      { id: 3, movie_id: 1, genre_id: 6 }]
-
-    return db.getGenresId(movieId, testDb)
-      .then(actual =>
-        expect(actual).toEqual(expected)
-      )
-  })
-
-  it('getGenresByIds returns the correct genres', () => {
-    expect.assertions(1)
-
-    const genreIds = [ 5, 7 ]
-    const expected = [ { id: 5, type: 'Comedy' },
-      { id: 7, type: 'Thriller' }]
-
-    return db.getGenresByIds(genreIds, testDb)
-      .then(actual =>
-        expect(actual).toEqual(expected)
-      )
-  })
-
-  it('getGenresByMovieId returns the correct genres', () => {
-    expect.assertions(1)
-
-    const movieId = 3
-    const expected = [ { id: 1, type: 'Action' },
-      { id: 2, type: 'Adventure' },
-      { id: 8, type: 'Fantasy' }
+    const moviesId = [2, 3, 4]
+    const expected = [
+      { name: 'The Incredible Hulk' },
+      { name: 'Thor' },
+      { name: 'The Godfather: Part II' }
     ]
 
-    return db.getGenresByMovieId(movieId, testDb)
+    return db.getMoviesByIds(moviesId, testDb)
       .then(actual =>
         expect(actual).toEqual(expected)
-      )
-  })
-
-  it('getMoviesByChosedTypes returns the recommended movie', () => {
-    expect.assertions(1)
-
-    const moviesId = [3, 5, 1]
-    const genre = 'Adventure'
-    const expected = { id: 2, name: 'The Incredible Hulk (2008)', image: '/images/2.png' }
-
-    return db.getMoviesByChosedTypes(genre, moviesId, testDb)
-      .then(actual =>
-        expect(actual[0]).toEqual(expected)
       )
   })
 })
--- tests/db/db/seeds/movies.js
+++ tests/db/db/seeds/movies.js
@@ -1,41 +1,10 @@
 exports.seed = (knex) =>
-  knex('movies_genres').del()
-    .then(() => knex('movies').del())
-    .then(() => knex('genres').del())
+  knex('movies').del()
     .then(() =>
       knex('movies').insert([
-        { id: 1, name: 'Iron Man (2008)', image: '/images/1.png' },
-        { id: 2, name: 'The Incredible Hulk (2008)', image: '/images/2.png' },
-        { id: 3, name: 'Thor (2011)', image: '/images/1.png' },
-        { id: 4, name: 'The Godfather: Part II  (1974)', image: '/images/1.png' },
-        { id: 5, name: 'The Lives of Others  (2006)', image: '/images/1.png' }
-      ]))
-    .then(() =>
-      knex('genres').insert([
-        { id: 1, type: 'Action' },
-        { id: 2, type: 'Adventure' },
-        { id: 3, type: 'Animation' },
-        { id: 4, type: 'Biography' },
-        { id: 5, type: 'Comedy' },
-        { id: 6, type: 'SciFi' },
-        { id: 7, type: 'Thriller' },
-        { id: 8, type: 'Fantasy' },
-        { id: 9, type: 'Crime' },
-        { id: 10, type: 'Drama' }
-      ]))
-    .then(() =>
-      knex('movies_genres').insert([
-        { id: 1, movie_id: 1, genre_id: 1 },
-        { id: 2, movie_id: 1, genre_id: 2 },
-        { id: 3, movie_id: 1, genre_id: 6 },
-        { id: 4, movie_id: 2, genre_id: 1 },
-        { id: 5, movie_id: 2, genre_id: 2 },
-        { id: 6, movie_id: 2, genre_id: 6 },
-        { id: 7, movie_id: 3, genre_id: 1 },
-        { id: 8, movie_id: 3, genre_id: 2 },
-        { id: 9, movie_id: 3, genre_id: 8 },
-        { id: 10, movie_id: 4, genre_id: 9 },
-        { id: 11, movie_id: 4, genre_id: 10 },
-        { id: 12, movie_id: 5, genre_id: 7 },
-        { id: 13, movie_id: 5, genre_id: 19 }
+        { id: 1, name: 'Iron Man', image: '/images/1.png' },
+        { id: 2, name: 'The Incredible Hulk', image: '/images/2.png' },
+        { id: 3, name: 'Thor', image: '/images/1.png' },
+        { id: 4, name: 'The Godfather: Part II', image: '/images/1.png' },
+        { id: 5, name: 'The Lives of Others', image: '/images/1.png' }
       ]))
