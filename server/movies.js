0425480d33a03427bbb9d9a6b59583c52a5b3f42
Sunny Niu <sunnyyingli@gmail.com>
2020-01-11 22:33:56 +1300

    delete unneeded files

--- server/movies.js
+++ server/movies.js
@@ -3,6 +3,7 @@ const express = require('express')
 const db = require('../db/db')
 
 const router = express.Router()
+
 router.use(express.json())
 
 const url = 'https://tastedive.com/api/similar?q='
@@ -11,44 +12,48 @@ const APIKey = '352362-MovieRec-XIQL1KMA'
 function getRandomInt (min, max) {
   min = Math.ceil(min)
   max = Math.floor(max)
+
   return Math.floor(Math.random() * (max - min)) + min
 }
 
 router.get('/movie', (req, res) => {
-  const moviesId = req.query.moviesId ? req.query.moviesId.split(',').map(item => parseInt(item, 10)) : []
+  const moviesId = req.query.moviesId ? req.query.moviesId.split(',').map((item) => parseInt(item, 10)) : []
 
   let id = getRandomInt(1, 90)
+
   while (moviesId.includes(id)) {
     id = getRandomInt(1, 90)
   }
 
   db.getMovieById(id)
-    .then(movie => res.json(movie))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR:' + err.message)
-    })
+  .then((movie) => res.json(movie))
+  .catch((err) => {
+    res.status(500).send(`DATABASE ERROR:${err.message}`)
+  })
 })
 
 router.get('/movies/:moviesId', (req, res) => {
   const moviesId = req.params.moviesId
+
   db.getMoviesByIds(moviesId)
-    .then(names => res.json(names))
-    .catch(err => {
-      res.status(500).send('DATABASE ERROR:' + err.message)
-    })
+  .then((names) => res.json(names))
+  .catch((err) => {
+    res.status(500).send(`DATABASE ERROR:${err.message}`)
+  })
 })
 
 router.get('/recommendmovies/:likedmovies', (req, res) => {
   const likedmovies = req.params.likedmovies
+
   request
-    .get(`${url}${likedmovies}&info=1&type=movie&verbose=1&k=${APIKey}`)
-    .end((err, result) => {
-      if (err) {
-        res.status(500).send(err.message)
-      } else {
-        res.json(result.body)
-      }
-    })
+  .get(`${url}${likedmovies}&info=1&type=movie&verbose=1&k=${APIKey}`)
+  .end((err, result) => {
+    if (err) {
+      res.status(500).send(err.message)
+    } else {
+      res.json(result.body)
+    }
+  })
 })
 
 module.exports = router
